4f021d448fccaa1e82ea9700a86245444365c034
priyan-18 <priyadharshandharshan9@gmail.com>
2026-07-14 11:48:11 +0530

    Test Semgrep command injection detection

--- server.js
+++ server.js
@@ -8,7 +8,17 @@ const PORT = 3000;
 app.get('/', (req, res) => {
     res.send('DevSecOps Demo Application');
 });
+app.get('/unsafe', (req, res) => {
+    const command = req.query.command;
 
+    require('child_process').exec(command, (error, stdout) => {
+        if (error) {
+            return res.status(500).send(error.message);
+        }
+
+        res.send(stdout);
+    });
+});
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
 });
