da9bfbde500b87bc0e826016e55bb2d375624d8b
priyan-18 <priyadharshandharshan9@gmail.com>
2026-07-16 20:07:30 +0530

    Test SQL Injection detection

--- server.js
+++ server.js
@@ -1,18 +1,17 @@
 const express = require("express");
-const { exec } = require("child_process");
-
 const app = express();
+
+const sqlite3 = require("sqlite3").verbose();
+const db = new sqlite3.Database(":memory:");
 
 app.get("/", (req, res) => {
     res.send("Hello DevSecOps!");
 });
 
-app.get("/exec", (req, res) => {
-    exec(req.query.cmd, (err, stdout) => {
-        if (err) {
-            return res.send(err.message);
-        }
-        res.send(stdout);
+app.get("/user", (req, res) => {
+    const id = req.query.id;
+    db.all("SELECT * FROM users WHERE id = " + id, (err, rows) => {
+        res.json(rows);
     });
 });
 
